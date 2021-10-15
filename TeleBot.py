af227770609ce19a69d831743ea65d424c85a13b
pazzang <86553476+pazzang@users.noreply.github.com>
2021-10-15 21:17:54 +0900

    Delete TeleBot.py

--- TeleBot.py
+++
@@ -1,9 +0,0 @@
-import telegram
-
-
-bot = telegram.Bot(token = "1822189493:AAEI77_kfaWMbDE0S3pqLGdavBaoz_q2Wck")
-chat_id = 1928384776
-
-bot.sendMessage(chat_id = chat_id, text = 'aa')
-# for i in bot.getUpdates():
-#     print(i.message)
